09f976e7bd59ec33c4389093fd03ef13369a7230
Paul chabinga <paulchabinga@Pauls-MacBook.local>
2018-01-19 22:11:57 +0000

    Template for Node React

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div className="btn btn-outline-primary btn-lg btn-block">
+     Template for React and Node js Projects
+    </div>
+  );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
